fe01ab12c452d26ed1cbe0494ce64ee4add029bd
Rayyan Borg <42804334+MrRayyanBorg@users.noreply.github.com>
2021-08-16 15:59:21 +0000

    Initial F_to_C / C_to_F Primer

--- include/monday.h
+++ include/monday.h
@@ -1,6 +1,40 @@
+#include <iostream>
+
+float f_to_c(float f) {
+  return ((f - 32) * 5) / 9; //Fahrenheit to Centigrade.
+}
+
+float c_to_f(float c) {
+  return ((c * 9) / 5) + 32; //Centigrade to Fahrenheit.
+}
+
 void fahrenheitCentigradeConversion(void) {
-	std::cout << " - fahrenheitCentigradeConversion: not yet implemented\n\n";
-  std::cout << "updated";
+  std::string temp;
+  std::string choice;
+  std::string label;
+  float input_temp = 0.0;
+  float result_temp = 0.0;
+
+  std::cout << "\nPlease enter the starting temperature: ";
+  std::getline(std::cin, temp);
+  input_temp = stof(temp);
+
+  std::cout << "\nPress ‘C’ to convert from Fahrenheit to Centigrade";
+  std::cout << "\nPress ‘F’ to convert from Centigrade to Fahrenheit.\n\n";
+  std::cout << "\nYour choice: ";
+  std::getline(std::cin, choice);
+  
+  if(choice=="c" || choice=="C") {
+    result_temp =  f_to_c(input_temp);
+    label = "Farenheit";
+  }
+
+  if(choice=="f" || choice=="F") {
+    result_temp = c_to_f(input_temp);
+    label = "Centigrade";
+  }
+
+  std::cout << "\n" << input_temp <<" degrees "  << label <<" is = " << result_temp;
 }
 
 
