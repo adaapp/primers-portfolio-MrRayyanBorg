e0afa9a660437d8476e2e80883ce23619348d0e4
Rayyan Borg <42804334+MrRayyanBorg@users.noreply.github.com>
2021-08-17 15:26:36 +0000

    Added Primer3.

--- include/tuesday.h
+++ include/tuesday.h
@@ -1,5 +1,46 @@
-void passwordComplexityChecker(void) {
-	std::cout << " - passwordComplexityChecker: not yet implemented\n\n";
+#include <iostream>
+#include <ctype.h>
+
+void passwordComplexityChecker() {
+  std::string password;
+  std::cout  << "Password: ";
+  std::cin >> password;
+
+  int alpha_chars = 0;
+  int digits = 0; 
+  int special_chars = 0;
+  int password_length = password.length();
+  std::string result;
+
+  for (int i = 0; i < password_length; i++){
+    if(isalpha(password[i])){
+      alpha_chars++;
+    }
+    else if(isdigit(password[i])){
+      digits++;
+    }
+    else{
+      special_chars++;
+    }
+  }
+
+  if(password_length >= 8 && digits >= 2 && alpha_chars >= 4  && special_chars >= 1){
+    result = "Very Strong";
+  }
+  else if(password_length >= 8 && digits >= 2 && alpha_chars >= 4){
+    result = "Strong";
+  }
+  else if(password_length >= 4 && digits >= 1 && alpha_chars >= 1){
+    result = "Moderate";
+  }
+  else if (password_length >= 1 && ((digits >= 1 && alpha_chars == 0) || (digits == 0 && alpha_chars >= 1))){
+    result = "Weak";
+  }
+  else{
+    result = "That password was invalid.";
+  }
+
+  std::cout << "The password '" << password << "' is " << result;
 }
 
 
